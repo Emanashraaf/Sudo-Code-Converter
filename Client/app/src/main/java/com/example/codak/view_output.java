e55e59da92e4a75a80c9c863b96b692db5e48075
Emanashraaf <eman.ashraf@yahoo.com>
2019-05-06 18:07:52 +0200

    Add Existing File

--- Client/app/src/main/java/com/example/codak/view_output.java
+++ Client/app/src/main/java/com/example/codak/view_output.java
@@ -1,25 +1,132 @@
 package com.example.codak;
 
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.LinearLayout.LayoutParams;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
-public class view_output extends AppCompatActivity {
+public class view_output extends AppCompatActivity
+{
+    TextView code , output;
+    LinearLayout ll;
+    LayoutParams lp;
 
-    TextView code;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_output);
         code = (TextView) findViewById(R.id.code);
-        File file = new File(MainActivity.outputfile_path);
-        try {
+        output = (TextView) findViewById(R.id.output);
+        File file = new File(CodakActivity.outputfile_path);
+        try
+        {
             code.setText(new Scanner(file).useDelimiter("\\Z").next());
-        } catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e)
+        {
             e.printStackTrace();
         }
+    }
+
+    Thread thread = new Thread(new Runnable()
+    {
+        @Override
+        public void run() {
+            try {
+                ll = (LinearLayout) findViewById(R.id.ll);
+                final LayoutParams lp = new LinearLayout.LayoutParams(
+                        LayoutParams.WRAP_CONTENT, // Width of TextView
+                        LayoutParams.WRAP_CONTENT); // Height of TextView
+                lp.setMargins(10,10,10,10);
+
+                CodakActivity.dos.write("run".getBytes());
+                byte[] in_b = new byte[500];
+                String in;
+
+                int size = CodakActivity.dis.read(in_b);
+                in = new String(in_b, 0, size, "UTF-8");
+                CodakActivity.dos.write("-1".getBytes());
+
+                while (!in.equals("end")) {
+                    final String finalIn = in;
+                    Log.w("Tag",finalIn);
+                    runOnUiThread(new Runnable() {
+                        @SuppressLint("ResourceAsColor")
+                        public void run() {
+                            if (finalIn.equals("input"))
+                            {
+                                final EditText et = new EditText(getBaseContext());
+                                et.setLayoutParams(lp);
+                                et.setTextSize(15f);
+                                et.setTextColor(R.color.silver);
+                                et.setBackgroundResource(R.color.white);
+                                et.setSelection(0);
+                                et.setHint("Enter input");
+                                ll.addView(et);
+
+
+                                Button ok = new Button(getBaseContext());
+                                ok.setLayoutParams(lp);
+                                ok.setText("Send");
+                                ok.setTextSize(15f);
+                                ok.setTextColor(Color.WHITE);
+                                ok.setBackgroundResource(R.color.ko7ly);
+                                ll.addView(ok);
+                                ok.setOnClickListener(new View.OnClickListener() {
+                                    public void onClick(View v) {
+                                        String input = et.getText().toString();
+                                        try {
+                                            CodakActivity.dos.write(input.getBytes());
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                });
+
+                            }
+                            else
+                                {
+                                TextView tv = new TextView(getBaseContext());
+                                tv.setLayoutParams(lp);
+                                tv.setTextSize(15f);
+                                tv.setTextColor(R.color.silver);
+                                tv.setText(finalIn);
+                                ll.addView(tv);
+                            }
+                        }
+                    });
+
+                    in_b = new byte[500];
+                    size = CodakActivity.dis.read(in_b);
+                    in = new String(in_b, 0, size, "UTF-8");
+                    CodakActivity.dos.write("-1".getBytes());
+                }
+                CodakActivity.client.close();
+            }
+
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    });
+
+    public void execute(View v)
+    {
+        output.setVisibility(View.VISIBLE);
+        thread.start();
     }
 }
--- Server/codak.py
+++ Server/codak.py
@@ -1,39 +1,60 @@
-import socket
 import pytesseract
 import cv2
 from PIL import Image
 import matplotlib as plt
+import numpy as np
+import socket
 import os
 from _thread import *
-import numpy as np
 
+
+# Extract text from image
+#please check your path of tesseract.exe on your machine 
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 tessdata_dir_config = r'--tessdata-dir "C:\\Program Files\\Tesseract-OCR\\tessdata"'
 
-# Extract text from image
+"""this function to extract text from the input image ,
+   after resizing it and connvert it to grayscale ,
+   then threshold it using binary thresholding and otsu ,
+   then extract the text using tesseract """
+
 def extract_text(img):
     img = cv2.resize(img,(1000,1000))
     img = np.array(img,np.uint8)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     val,binary = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    text = pytesseract.image_to_string(binary)
-    print(text)
+    text = pytesseract.image_to_string(Image.fromarray(binary),lang='eng',config=tessdata_dir_config)
+    
+    #sudo.txt file will have the code from the input image
     f = open("sudo.txt","w+")
     f.write(text)
     f.close()
+ 
+# Convert Sudo Code
+
+# dictionaries to convert inputes to its equivalents in python 
 
 key_word={"Display":"print","Promot":"input()","print":"print"}
 string_operations={"add":"+","sub":"-","mul":"*","div":"/","mod":"%" ,"equal" : "=" , "greater_than" : ">" ,"less_than" : "<","greater_than_or_equal" : ">=",
                    "less_than_or_equal" : "=<" ,"not_equal":"!="}
 operations = ["+" , "-" , "*" , "/"] 
 key=["number","string","to","do"]
-numbers ={"zero":0 , "one":1 ,"two":2 , "three":3 ,"four":4 ,"five":5 ,"six":6 ,"seven":7 ,
-          "eight":8 ,"nine":9 ,"ten":10}
 
-def compile_sudo (input_ , result,exec_file):
+key_word={"Display":"print","Promot":"input()","print":"print"}
+string_operations={"add":"+","sub":"-","mul":"*","div":"/","mod":"%" ,"equal" : "=" , "greater_than" : ">" ,"less_than" : "<","greater_than_or_equal" : ">=",
+                   "less_than_or_equal" : "=<" ,"not_equal":"!="}
+operations = ["+" , "-" , "*" , "/"] 
+key=["number","string","to","do"]
+
+# compiler function to python code
+""" arguments -->
+    input_ : this is the input file of sudocode resulted from preprocessing stage
+    result : the output file which contains python code will be appear to the user and comiled
+    exec_file : this file contains the code will send to the client from server in the background
+    """
+def compile_sudo (input_ , result, exec_file):
     indent = 0
     text = input_.readline().rstrip('\n')
-    
     while text:        
         text_list = text.split()
         if ((text_list[0]) == ("endfor")) or ((text_list[0]) == ("endif")) :
@@ -43,17 +64,49 @@ def compile_sudo (input_ , result,exec_file):
 
         if ((text_list[0]) == ("else")) or ((text_list[0]) == ("elseif")) :
             indent = indent - 1
-                
-        result.write("   " * indent)
-        print("   " * indent ,end = "")
         
+        #this is because the indents python take instead of brackets
+        result.write("   " * indent)
+        exec_file.write("   " * indent)
+        print("   " * indent ,end = "")
+ 
+        #Display use to print strings 
         if text_list[0] == "Display" :
             str1 = ' '.join(text_list[1:])
             print(key_word[text_list[0]],'("',str1,'")')
-            result.write("%s ( '"'%s'"' )\r\n" %(key_word[text_list[0]],str1))
+            result.write("%s ( '"'%s'"') \r\n" %(key_word[text_list[0]],str1))
+            exec_file.write("f = open('out.txt','w+')\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("%s ('"'%s'"' , file = f ) \r\n" %(key_word[text_list[0]],str1))
+            exec_file.write("   " * indent)
+            exec_file.write("f.close()\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("f = open('out.txt','r+')\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("s = f.readline().rstrip('\\n ')\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("print(s)\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("c.send(bytes(s,'utf8'))\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("print(bytes(s,'utf8'))\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("ack=c.recv(1024)\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("f.truncate(0)\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("f.close()\r\n")
+            
+        #promot use to take input from user
         elif text_list[0] == "Promot" :
-            exec_file.write("c.send('input')")
+            result.write('print("Please Enter your input:")\r\n')
+            exec_file.write("c.send(bytes('Please Enter your input:','utf8'))\r\n")
+            exec_file.write("c.send(bytes('input','utf8'))\r\n")
+            exec_file.write("ack=c.recv(1024)\r\n")
+            exec_file.write("user_input=c.recv(1024).decode()\r\n")
+
             temp= key_word[text_list[0]]
+            #this if when the user input interger number
             if(key[0] in text_list):
 
                 text = input_.readline().rstrip('\n')
@@ -62,81 +115,126 @@ def compile_sudo (input_ , result,exec_file):
                     str1 = text_list[-1]
                     print(str1,"=int(",temp,")")
                     result.write("%s =int( %s )\r\n" %(str1,temp))
-            else:
+                    exec_file.write("%s =int( user_input )\r\n" %(str1))
+                    
+            #this if when the user input string
+            else :
                 if text_list[0] == "Save" :
                     str1 = text_list[-1]
                     print(str1,"=",temp)
                     result.write("%s = %s \r\n "%(str1,temp))
+                    exec_file.write("%s = user_input \r\n "%(str1))
 
+        #print use to print the output to the user in ui             
         elif text_list[0] == "print" :
             str1 = ' '.join(text_list[1:])
             print(key_word[text_list[0]],'(',str1,')')
             result.write("%s ( %s )\r\n"%(key_word[text_list[0]],str1))
-
+            exec_file.write("f = open('out.txt','w+')\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("%s (%s  , file = f ) \r\n" %(key_word[text_list[0]],str1.lstrip()))
+            exec_file.write("   " * indent)
+            exec_file.write("f.close()\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("f = open('out.txt','r+')\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("s = f.readline().rstrip('\\n ')\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("print(s)\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("c.send(bytes(s,'utf8'))\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("print(bytes(s,'utf8'))\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("ack=c.recv(1024)\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("f.truncate(0)\r\n")
+            exec_file.write("   " * indent)
+            exec_file.write("f.close()\r\n")
+        
+        #the operation dictionary for equation statments
         elif([i for i in operations if i in text_list]):
             temp=' '.join(text_list)
             print(temp)
             result.write("%s\r\n"%(temp))
-
+            exec_file.write("%s\r\n"%(temp))
+        
+        #initialize use to initialize lists with fixed size 100
         elif text_list[0] == "initialize" :
-            print(text_list[1] , "= []")
-            result.write("%s = []\r\n"%(text_list[1]))
+            print(text_list[1] , "= [0]*100")
+            result.write("%s = [0]*100 \r\n"%(text_list[1]))
+            exec_file.write("%s = [0]*100 \r\n"%(text_list[1]))
 
+        #set use to initialize variables      
         elif text_list[0] == "set" :
             item = text_list.index(next(i for i in key if i in text_list))
             if(text_list[item]=="to"):
                 print(numbers[text_list[item+1]])
-                result.write("%s = %s\r\n"%(text_list[1] ,numbers[text_list[item+1]] ))
-                    
+                result.write("%s = %s \r\n"%(text_list[1] ,numbers[text_list[item+1]] ))
+                exec_file.write("%s = %s\r\n"%(text_list[1] ,numbers[text_list[item+1]] ))
 
-
+        #for loop in format of "for i in range(x,y):"
         elif text_list[0] == "for" :
-            indent =  indent + 1
+            indent =  indent + 1    #increase the indent counter after for statement
             iterator = text_list[1]
             item = text_list.index(next(i for i in key if i in text_list))
             if(text_list[item]=="to"):
                 print("for" , iterator ,"in range(",text_list[item-1],',',int(text_list[item+1])+1,'):')
                 result.write("for %s in range (%s,%d) :\r\n" %( iterator ,(text_list[item-1]),(int(text_list[item+1])+1)))
-
-
+                exec_file.write("for %s in range (%s,%d) :\r\n" %( iterator ,(text_list[item-1]),(int(text_list[item+1])+1)))
+                
+        #this is for if condition and support nested ifs inside it
         elif ((text_list[0]) == ("if")) or ((text_list[0]) == ("elseif")) or ((text_list[0]) == ("elif")) or ((text_list[0]) == ("while")) :
-            indent =  indent + 1
+            
+            indent =  indent + 1    #increase the indent counter after for statement
+            #execlude to and is from input sudo statement
             if "is" in text_list:
                 text_list.remove("is")
             if "to" in text_list:
                 text_list.remove("to")
+            # this to handle if the user want to write math operators in english words 
             if([i for i in string_operations if i in text_list]):
                 item = next(i for i in text_list if i in string_operations)
-                if item == "=" :
-                   text_list[text_list.index(item)] = string_operations[item]+string_operations[item]
+                if item == "equal" :
+                    text_list[text_list.index(item)] = string_operations[item]+string_operations[item]
                 else :
-                   text_list[text_list.index(item)] = string_operations[item] 
+                    text_list[text_list.index(item)] = string_operations[item]
             elif ("=" in text_list):
                 text_list[text_list.index("=")] = "=="
-                
+
             if(text_list[0]) == ("elseif"):
                 text_list[0] = "elif"
             temp = ' '.join(text_list)
             print(' '.join(text_list) , ":")
             result.write("%s :\r\n"%(temp))
-            
-                
+            exec_file.write("%s :\r\n"%(temp))  
+
         elif text_list[0] == "else" :
             print ("else :")
             result.write("else :\r\n")
+            exec_file.write("else :\r\n")
             indent =  indent + 1
-                
+        
+        #reading new line in input file
         text = input_.readline().rstrip('\n')
-
-        if(len(text)==0):
+        
+        #handling the case if there is multi empty lines between our code because of the tesseract output
+        if(len(text) == 0):
             text = input_.readline().rstrip('\n')
-            
+
+    
+    exec_file.write("c.send(bytes('end','utf8'))\r\n") 
+    # close files and return result file to execute
     result.close()
+    exec_file.close()
     return result
+
+
+#Conversion Process
 
 def threaded(c,addr):
     # Recieve image from client
-    with open('tst.jpg', 'wb') as img:
+    with open('input.jpg', 'wb') as img:
         data = c.recv(1024)
         while data != b'done':
             print(data)
@@ -145,14 +243,14 @@ def threaded(c,addr):
     print ('image is recieved!')
 
     # Extract text from image
-    img = cv2.imread('tst1.jpg')
+    img = cv2.imread('input.jpg')
     extract_text(img)
 
     # Convert sudo code
     input_file = open("sudo.txt" , "r+")
     output_file = open("result.txt" , "w+")
     exec_file = open("exec.txt" , "w+")
-    print('start compilation')
+    print('Start compilation')
     compile_sudo(input_file,  output_file , exec_file)
 
     # Send output to client
@@ -161,20 +259,31 @@ def threaded(c,addr):
     c.send(f.read(4096))
     f.close()
     print("Transfer Complete")
+
+    # Execute Code
+    run = c.recv(1024)
+    if (run== b'run'):
+        exec(open(exec_file.name).read())
+
+    #close socket
+    c.close()
     
+
+# Main Function
 
 def Main():
     # Create a Server Socket
     server = socket.socket()
     host= socket.gethostbyname(socket.gethostname()) 
-    port = 12345
+    port = 12346
     server.bind((host, port))
-
+    print(host)
     # Wait for client connection
     server.listen()
     while True:
         print ('Server is waiting..')
         client, addr = server.accept()
+        
         print ('Got connection from', addr)
 
         # Start new thread
